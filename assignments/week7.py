975f31c0fc00d816be75831daa9e58fe08977974
jembrown <jeremymbrown@gmail.com>
2018-12-08 16:08:03 -0600

    Comments added

--- assignments/week7.py
+++ assignments/week7.py
@@ -10,14 +10,18 @@ print (len(dnaSeq))
 
 #use this to count number of As
 print  ("Number of As:")
-print (dnaSeq.count('a'))
+print (dnaSeq.lower().count('a'))
 
 print ("Number of C's")
-print (dnaSeq.count ('c'))
+print (dnaSeq.lower().count ('c'))
 
 print ("Number of Gs")
-print (dnaSeq.count ('g'))
+print (dnaSeq.lower().count ('g'))
 
 print ("Number of Ts")
-print (dnaSeq.count ('t'))
+print (dnaSeq.lower().count ('t'))
 
+# DB: Good, although this won't count properly if the sequence is provided in uppercase.
+#     One way to avoid this problem is to add .lower() after dnaSeq (see above). Also, it's
+#     a good idea to only use .sh in file names for shell scripts. Instead, use .py for
+#     Python code.
